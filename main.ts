eb044fda82e0ae43066226db0b74315b91afde83
redfish2022 <114443188+redfish2022@users.noreply.github.com>
2022-09-26 15:39:08 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(1)
+    basic.showNumber(2)
+    basic.showNumber(3)
+    basic.showNumber(4)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Heart)
+})
